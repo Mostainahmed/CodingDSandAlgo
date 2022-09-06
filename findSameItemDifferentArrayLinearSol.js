6adca6f59ae4fed572075b327029a3f5d813d32a
Mostainahmed <mostainahmed@gmail.com>
2022-09-06 22:04:51 +0200

    finding common items solved with O(a+b) time complexity

--- findSameItemDifferentArrayLinearSol.js
+++ findSameItemDifferentArrayLinearSol.js
@@ -1,6 +1,21 @@
+function findCommonItem(array1, array2){
+    let map = {}
+    for (let i = 0; i<array1.length; i++) {
+        if(!map[i]){
+            const item = array1[i];
+            map[item] = true;
+        }
+    }
+
+    for (let j = 0; j<array2.length; j++) {
+        if(map[array2[j]]) {
+            console.log("true"+" "+array2[j])
+        }
+    }
+}
 const array1 = ["a","x","y","z"]
 const array2 = ["p","q","a"]
-console.log("hello")
+findCommonItem(array1, array2);
 
 
 
