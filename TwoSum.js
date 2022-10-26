bfc5955e9d089cc4593859e770928d66b81a0c84
Mostainahmed <mostainahmed@gmail.com>
2022-10-26 02:01:59 +0200

    Solving first leetcode problem

---
+++ TwoSum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+    
+};
+
+let nums = [1,2,3]
+let target = 5
+twoSum(nums, target)
